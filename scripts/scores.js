1b37ae20f7b227589b4785310a6029c5a758b41b
Vishnu Madhusoodanan <vmadhusoodanan95@gmail.com>
2018-03-10 14:05:39 -0600

    Fix date formatting for url

--- scripts/scores.js
+++ scripts/scores.js
@@ -2,7 +2,15 @@
 // Example URL: http://stats.nba.com/stats/scoreboard/?GameDate=02/22/2018&LeagueID=00&DayOffset=0
 function getScoreboardUrl(date) {
     var year = date.getFullYear();
-    return 'http://stats.nba.com/stats/scoreboard/?GameDate=' +  + '&LeagueID=00&DayOffset=0';
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    if(day < 10) {
+    	day = "0" + day;
+    }
+    if (month < 10) {
+    	month = "0" + month;
+    }
+    return 'http://stats.nba.com/stats/scoreboard/?GameDate=' + month + "/" + day + "/" + year + '&LeagueID=00&DayOffset=0';
 }
 
 function sendScoreboard(scoreboard) {
